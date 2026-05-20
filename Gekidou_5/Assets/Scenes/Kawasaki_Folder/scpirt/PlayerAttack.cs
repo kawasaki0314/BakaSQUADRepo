8b771a1eaa3aa093b25072aac7d8487d152d2f64
kawasaki0314 <fko2647038@stu.o-hara.ac.jp>
2026-05-20 18:44:29 +0900

    20260520_kawasaki_kaitenkougeki

--- Gekidou_5/Assets/Scenes/Kawasaki_Folder/scpirt/PlayerAttack.cs
+++ Gekidou_5/Assets/Scenes/Kawasaki_Folder/scpirt/PlayerAttack.cs
@@ -5,9 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject attackPrefab; // 攻撃プレハブ
+    public GameObject orbitPrefab; // 攻撃プレハブ２
     public float attackOffset = 1.0f; // 前に出す距離
 
     private PlayerMove playerMove;
+
+    private float playTime = 0f; // 時間管理
+
+    // 一度だけ生成するためのフラグ
+    private bool orbitSpawned = false;
 
     void Start()
     {   // PlayerMoveを取得
@@ -15,7 +21,18 @@ public class PlayerAttack : MonoBehaviour
     }
 
     void Update()
-    {   // 左クリックで攻撃
+    {
+        // 経過時間を増やす
+        playTime += Time.deltaTime;
+
+        // 一定時間で回転攻撃を出す(1回のみ)
+        if (playTime >= 30f && !orbitSpawned)
+        {
+            orbitSpawned = true;
+            SpawnOrbitAttack();
+        }
+
+        // 左クリックで攻撃
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Attack();
@@ -38,5 +55,16 @@ public class PlayerAttack : MonoBehaviour
 
         // 攻撃の生成
         Instantiate(attackPrefab, spawnPos, rot);
+    }
+
+    void SpawnOrbitAttack()
+    {
+        GameObject orbit = Instantiate(orbitPrefab, transform.position,
+            Quaternion.identity);
+
+        AttackOrbit atk = orbit.GetComponent<AttackOrbit>();
+
+        // プレイヤーを中心にする
+        atk.player = transform;
     }
 }
