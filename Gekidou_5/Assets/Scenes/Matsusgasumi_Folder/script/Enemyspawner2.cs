abc4c2c78128909c05bd53c3ac1e034a80f4eb4b
matugasumikaito <fko2647002@stu.o-hara.ac.jp>
2026-05-25 18:19:53 +0900

    20260525_ma

--- Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/Enemyspawner2.cs
+++ Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/Enemyspawner2.cs
@@ -1,74 +1,50 @@
 using System.Collections;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class Enemyspawner : MonoBehaviour
+public class EnemySpeawner2: MonoBehaviour
 {
-    //【追加】これがないとAIHoming側から「Enemyspawner.Instance」で呼べません
-    //   public static Enemyspawner Instance { get; private set; }
     //ここ(関数の外)に書くことで、スクリプト内のどこからでも使えるようになります!
     [SerializeField] GameObject EnemyPrefab;
     [SerializeField] float SpawnInterVal = 2.0f; //  少し間隔を広げて2秒ごとに設定
 
-    //===追加===
     [SerializeField] int maxEnemyCount = 15;//画面に存在できる敵の最大数
-    /*
-    //ゲーム開始時に実体を登録
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-    */
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+    void Start()
     {
-        // 定期生成を行うコルーチンを開始
+        //定期生成を行うコルーチン開始
         StartCoroutine(SpawnRoutine());
     }
-
-    //2. 足りなかったコルーチン本体です
-    private IEnumerator SpawnRoutine()
-
+//2. 足りなかったコルーチン本体です
+private IEnumerator SpawnRoutine()
     {
-        while (true)
-
+        while(true)
         {
             yield return new WaitForSeconds(SpawnInterVal);
 
             //タグを使って、今画面にいる敵の数を数える
             int currentEnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
 
-
-            //もし最大数を超えていたら、この回はスポンをスキップする
-            if (currentEnemyCount >= maxEnemyCount)
-
+            //もし最大数を超えていたら、この回はスキップする
+            if(currentEnemyCount >= maxEnemyCount)
             {
                 continue;
             }
+        }
+        /*
+        //最大数に余裕がある分だけ、あるいは最大5匹生み出す
+        for (int i = 0; i < 5; i++)
 
+        {
+            //生成する直前にもう一度上限チェック
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length >= maxEnemyCount) break;
 
-            //最大数に余裕がある分だけ、あるいは最大5匹生み出す
-            for (int i = 0; i < 5; i++)
-
-            {
-                //生成する直前にもう一度上限チェック
-                if (GameObject.FindGameObjectsWithTag("Enemy").Length >= maxEnemyCount) break;
-
-                SpawnEnemy();
-
-            }
+            SpawnEnemy();
 
         }
+        */
 
     }
-
     //3. 足りなかった「実際に敵を生み出す処理」です
     private void SpawnEnemy()
 
@@ -96,3 +72,4 @@ public class Enemyspawner : MonoBehaviour
     }
 
 }
+
