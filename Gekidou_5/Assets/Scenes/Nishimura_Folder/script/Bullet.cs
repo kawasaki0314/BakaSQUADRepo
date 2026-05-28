a3526473c66c691f75d1eef3b15156a054372205
KIRAGRA <fko2647040@stu.o-hara.ac.jp>
2026-05-28 18:55:04 +0900

    2026528ki

---
+++ Gekidou_5/Assets/Scenes/Nishimura_Folder/script/Bullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float speed = 1f;
+    Vector2 direction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
