fbfaca98aa35c58be82c0608e77d891ab3479b28
KIRAGRA <fko2647040@stu.o-hara.ac.jp>
2026-07-06 16:43:08 +0900

    kira

---
+++ Gekidou_5/Assets/Scenes/Nishimura_Folder/script/BgmManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32.SafeHandles;
+using UnityEditor;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour
+{
+    float timer = 0f;
+    [SerializeField]AudioSource tureaudio;
+    Transform playerTransform;
+    bool bgmDestroy = false;
+    //public GameObject BossPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 3f && !bgmDestroy)
+        {
+            Destroy(gameObject);
+            bgmDestroy = true;
+
+        }
+    }
+    //void BgmDestroy(GameObject gameObject)
+    //{
+    //    timer += Time.deltaTime;
+
+    //    if (timer < 1f && !bgmDestroy)
+    //    {
+    //        Destroy(gameObject);
+
+    //    }
+    //}
+}
