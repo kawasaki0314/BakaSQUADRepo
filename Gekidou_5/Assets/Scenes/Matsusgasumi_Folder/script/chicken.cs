4407a6e6e329f481a3c2bad97341115e5d993438
matugasumikaito <fko2647002@stu.o-hara.ac.jp>
2026-07-15 18:28:49 +0900

    20260715
    チキン作ります

---
+++ Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/chicken.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    private Vector2 pos;
+    public int num = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        //マイナスをかけることで逆方向にいどうする
+        transform.Translate(transform.right * Time.deltaTime * 3 * num);
+
+        if(pos.x > -2)
+        {
+            num = -1;
+        }
+        if(pos.x < -11)
+        {
+            num = 11;
+        }
+    }
+}
---
+++ Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/hp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class hp : MonoBehaviour
+{
+    [Header("HP Settings")]
+    [SerializeField] int maxHp = 1;
+    public int currentHp = 1;
+    
+
+    private chicken chicken;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHp = maxHp;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
