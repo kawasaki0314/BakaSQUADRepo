e1f960fecd17951761a38d87241c2cf490a57a9d
kawasaki0314 <fko2647038@stu.o-hara.ac.jp>
2026-07-16 18:45:25 +0900

    20260726_kawasaki

---
+++ Gekidou_5/Assets/Scenes/Kawasaki_Folder/scpirt/StatusGaugeUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class StatusGaugeUI : MonoBehaviour
+{
+    [SerializeField] private Image attackGauge;
+    [SerializeField] private Image speedGauge;
+    [SerializeField] private Image bulletGauge;
+    [SerializeField] private Image hpGauge;
+    [SerializeField] private Image fireRateGauge;
+
+    public void UpdateAttack(int value)
+    {
+        attackGauge.fillAmount = value/10f;
+    }
+    public void UpdateSpeed(float value)
+    {
+        speedGauge.fillAmount = value / 10f;
+    }
+    public void UpdateBullet(int value)
+    {
+        bulletGauge.fillAmount = value / 10f;
+    }
+    public void UpdateHP(int value)
+    {
+        hpGauge.fillAmount = value / 200f;
+    }
+    public void UpdateFireRate(float value)
+    {
+        fireRateGauge.fillAmount = value / 5f;
+    }
+}
