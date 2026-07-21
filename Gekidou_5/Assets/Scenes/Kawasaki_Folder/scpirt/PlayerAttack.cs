24e043ce1bd570d308afd0ef7ebc5e00b05295f1
soma1795 <fko2647023@stu.o-hara.ac.jp>
2026-07-21 18:32:24 +0900

    20260721_noda

--- Gekidou_5/Assets/Scenes/Kawasaki_Folder/scpirt/PlayerAttack.cs
+++ Gekidou_5/Assets/Scenes/Kawasaki_Folder/scpirt/PlayerAttack.cs
@@ -1,5 +1,4 @@
-﻿using UnityEditor.UIElements;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 
---
+++ Gekidou_5/Assets/Scenes/Noda_Folder/script/exit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // Unityエディタ上で実行中の場合、再生モードを停止する
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // ビルドされたゲーム（PC / Androidなど）を終了する
+            Application.Quit();
+#endif
+    }
+}
