67b102c0407ade501d57d14d79adafdadd3f575d
matugasumikaito <fko2647002@stu.o-hara.ac.jp>
2026-07-22 18:30:13 +0900

    20260722_ma
    チキンあともう少しでおわるかも？

--- Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/chicken.cs
+++ Gekidou_5/Assets/Scenes/Matsusgasumi_Folder/script/chicken.cs
@@ -23,7 +23,46 @@ public class chicken : MonoBehaviour
         }
         if(pos.x < -11)
         {
-            num = 11;
+            num = 1;
         }
     }
+
+    //死亡処理
+    public void Die()
+    {
+        // EnemySpawnクラスのInstance（自分自身）を直接呼ぶ
+        // ※もしクラス名が EnemySpeawn なら、ここも EnemySpeawn に合わせる
+        Debug.Log("敵を倒した!スポナーに補充を頼みます。");
+
+        //画面内からプレイヤーのレベルスクリプトを探して、経験値を手渡す
+        levelupplayer playerlevel = FindFirstObjectByType<levelupplayer>();
+        if (playerlevel != null)
+        {
+            playerlevel.Addexperience(1500);//敵を1体倒したら経験値「1500」手に入れる設定
+            Debug.Log("プレイヤーに経験値を1500あたえました！");
+        }
+        else
+        {
+            //もしもこのエラーが出てたら、プレイヤーに「PlayerLevel」スクリプトが付いているか確認してください
+            Debug.LogWarning("PlayerLevelスクリプトは見つかりません！経験値が加算さrませんでした。");
+        }
+
+        //Instance(シングルトン)を使ってスポナーに報告
+        //EnemySpawnのInstance(さっきAwakeで作ったやつ)を直接呼ぶ
+        if (spawn.Instance != null)
+        {
+            //倒された場所を伝えて補充してもらうう
+            spawn.Instance.OnEnemyDefeated(false, transform.position);
+            // Debug.Log("補充依頼しました");
+        }
+        else
+        {
+            //もしこれが出たら、Spawner側のAwakeが動いていない証拠です
+            Debug.LogError("spawnのInstanceが見つかりません！Spawnerオブジェクトにスクリプトを付け直してください");
+        }
+
+        //じぶんおｗ消去する処理は、すべての報告や処理が「終わったら最後」に1回だけ書くのが鉄則！
+        Destroy(gameObject);
+    }
+
 }
